7b3879599cb6bb412e4f5a171752a55fc068cb77
Yasmin El Mahdi <elmahdiyasmin@gmail.com>
2025-10-08 12:16:39 +0300

    Completed excercies 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
 
+    let wordsContainer = '';
+
+    if(num < 0) {
+        return 'ERROR';
+    }
+    else{
+        for(let index = 0; index < num; index++){
+
+        wordsContainer += word;
+
+    }
+    return wordsContainer;
+    }
 };
 
 // Do not edit below this line
